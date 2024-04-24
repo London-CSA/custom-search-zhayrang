d945b69daa4c089e3247f32ebe5a30c58317d9f8
zhayrang <153098065+zhayrang@users.noreply.github.com>
2024-04-24 12:21:09 +0000

    test message

--- static/script.js
+++ static/script.js
@@ -1,1 +1,14 @@
+function toggleButton(){
+    if(document.getElementById("txtSearch").value == "")
+    {
+        document.getElementById("btnSearch").disabled = true;
+    }
+    else{
+        document.getElementById("btnSearch").disabled = false;
+    }
+}
 
+function getDate(){
+    const d = new Date();
+    document.getElementById("date").innerHTML = d;
+}
